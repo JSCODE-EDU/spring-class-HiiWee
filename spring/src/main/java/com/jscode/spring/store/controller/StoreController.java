fd03e913ef8a6dc33a156b9ca3496f3b0a3ab231
HiiWee <wpdlzhf159@gmail.com>
2023-03-31 00:33:36 +0900

    feat: 상점 조회 기능 추가

--- spring/src/main/java/com/jscode/spring/store/controller/StoreController.java
+++ spring/src/main/java/com/jscode/spring/store/controller/StoreController.java
@@ -1,9 +1,13 @@
 package com.jscode.spring.store.controller;
 
+import com.jscode.spring.store.dto.StoreResponse;
 import com.jscode.spring.store.dto.StoreSaveRequest;
 import com.jscode.spring.store.dto.StoreSaveResponse;
+import com.jscode.spring.store.dto.StoresResponse;
 import com.jscode.spring.store.service.StoreService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,9 +36,17 @@ public class StoreController {
     /**
      * 전체 상점 조회 api
      */
+    @GetMapping("/stores")
+    public StoresResponse findAll() {
+        return storeService.findAll();
+    }
 
     /**
      * 상세 상점 조회 api
      */
+    @GetMapping("/stores/{storeId}")
+    public StoreResponse findById(@PathVariable final Long storeId) {
+        return storeService.findById(storeId);
+    }
 }
 
